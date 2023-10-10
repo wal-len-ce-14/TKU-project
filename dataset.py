80103fead3e2880d311effd22fefa5d7ce5fb724
wallence <a33369097@gmail.com>
2023-10-10 20:03:43 +0800

    add set_segmentation_model in train.py

--- dataset.py
+++ dataset.py
@@ -1,3 +1,60 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8797bb18bcd72a8682b082925fe4a6a20585bd21742f215b6275a7adfef2aa1b
-size 2088
+import os
+import cv2 as cv 
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+img_height = 224
+img_width = 224
+
+class data(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+        self.masks = os.listdir(mask_dir)
+    
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.masks[index])
+        image = np.array(cv.imread(img_path, cv.IMREAD_GRAYSCALE))
+        # image = np.array(cv.imread(img_path))
+        mask = np.array(cv.imread(mask_path, cv.IMREAD_GRAYSCALE), dtype=np.float32)
+
+        if self.transform:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        return image, mask
+
+class data_for_ben_or_mal(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+    def __len__(self):
+        return len(self.images)
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        image = np.array(cv.imread(img_path, cv.IMREAD_GRAYSCALE))
+        if "benign" in self.images[index]:
+            mask = torch.tensor([1,0,0])
+        elif "malignant" in self.images[index]:
+            mask = torch.tensor([0,1,0])
+        elif "normal" in self.images[index]:
+            mask = torch.tensor([0,0,1])
+        else:
+            print("data error")
+
+        if self.transform:
+            augmentations = self.transform(image=image)
+            image = augmentations["image"]
+        
+        return image, mask
+
+
