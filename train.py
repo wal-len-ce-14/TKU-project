57806c3d36477d0b7932ddd9bd1567d391e1191a
wallence <a33369097@gmail.com>
2023-10-10 22:15:00 +0800

    Optimize segmentation

--- train.py
+++ train.py
@@ -219,232 +219,27 @@ def train_ben_or_mal(
             logs.insert(tk.END, "\n")
             logs.configure(state="disabled")
 
+def log_record(logs, str):
+    if logs != '':
+        logs.configure(state="normal")
+        logs.insert(tk.END, str+'\n')
+        logs.configure(state="disabled")
+        logs.see(tk.END)
 
-def train(
-        model,
-        device="cpu",
-        epochs=5,
-        batch_size=64,
-        lr=0.001,
-        full_img="",
-        full_mask="",
-        img_height=224,
-        img_width=224,
-        show_dir="",
-        lit_n=0,
-        load_file='',
-        save="",
-        logs=None,
-        event_stop=None,
-        save_checkpoint=True,
-        val_precent=0.1
+def set_model( 
+    model,
+    batch_size=10,
+    lr=0.0001,
+    full_img="",
+    full_mask="",
+    show_dir='',
+    img_height=224,
+    img_width=224,
+    load='',
+    save='',
+    lit_n=0
 ):
-    try:
-        if load_file != '':
-            Load(model, torch.load(load_file))
-            print(f"load file from {load_file}")
-
-        print(f"====  Training   ====")
-        train_transform = A.Compose(
-            [
-                A.Resize(img_height, img_width),
-                ToTensorV2()
-            ],
-        )
-    #### Create data ####
-        full_data = data(full_img, full_mask, train_transform)
-        if lit_n != 0:
-            full_data, _ = random_split(full_data, [lit_n, len(full_data)-lit_n])
-        tr_s = int(len(full_data)*(1-val_precent))
-        
-        te_s = len(full_data) - tr_s
-        train_data, test_data = random_split(full_data, [tr_s, te_s])
-
-        train_Loader = DataLoader(train_data, batch_size, shuffle=True, drop_last=True)
-        test_Loader = DataLoader(test_data, batch_size, shuffle=False, drop_last=True)
-    #### optimizer, loss_function, ... ####
-
-        optimizer = optim.Adam(model.parameters(), lr)
-        loss_f = nn.CrossEntropyLoss() if model.n_classes > 1 else nn.BCEWithLogitsLoss()
-        global_step = 0
-        decrease = 10
-    #### training ####
-
-        for epoch in range(1, epochs+1):
-
-            if event_stop != None:
-                if event_stop.is_set() and event_stop != None:
-                    print("2stop happend!!!!")
-                    if logs != None:
-                        logs.configure(state="normal")
-                        logs.insert(tk.END, "[*] END\n")
-                        logs.configure(state="disabled")
-                    event_stop.clear()
-                    print(event_stop)
-                    break
-
-            print(f"====  epoch {epoch}   ====")
-            if logs != None:
-                logs.configure(state="normal")
-                logs.insert(tk.END, f"====  epoch {epoch}   ====\n")
-                logs.configure(state="disabled")
-            epoch_loss = 0
-            checkpoint = {'state_dict': model.state_dict(), 'optmizer': optimizer.state_dict()}
-            optimizer = optim.Adam(model.parameters(), lr)
-
-
-
-            for idx, (i_data, target) in enumerate(train_Loader):
-
-                if event_stop != None:
-                    if event_stop.is_set():
-                        print("stop happend!!!!")
-                        break
-
-                i_data = i_data.to(device, torch.float32)
-                target = target.to(device, torch.float32).unsqueeze(1)
-
-                prediction = model(i_data)
-                target = torch.where(target > 127, 1., 0.)
-                loss = loss_f(prediction, target)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                global_step += 1
-                epoch_loss += loss.item()
-
-                print(f"\t\tBatch {idx+1} done, with loss = {loss}")
-                if logs != None:
-                    logs.configure(state="normal")
-                    logs.insert(tk.END, f"[+] Batch {idx+1} done, with loss = {loss}\n")
-                    logs.configure(state="disabled")
-                    logs.see(tk.END)
-                # writer.add_scalar('loss', loss, epoch)                  # tensorboard 輸出畫線
-                
-            
-            # print(f"====  {epoch} end  ====")
-            print(f"[+] {epoch} epoch loss = {epoch_loss / len(train_Loader)}, lr = {lr}")
-            if logs != None:
-                logs.configure(state="normal")
-                logs.insert(tk.END, f"[+] {epoch} epoch loss = {epoch_loss / len(train_Loader)}, lr = {lr}\n")
-                logs.configure(state="disabled")
-                logs.see(tk.END)
-            
-            # if epoch == decrease:
-            #     if lr > 2e-5:
-            #         lr = lr*0.5
-            #         decrease *= 2
-            #     else:
-            #         lr = lr*0.8
-            #         decrease *= 3
-
-            if save_checkpoint:
-                Save(checkpoint, save)
-            #### check accurracy ####
-
-            print("====  check accurracy  ====\n")
-            if logs != None:
-                logs.configure(state="normal")
-                logs.insert(tk.END, "====  check accurracy  ====\n")
-                logs.configure(state="disabled")
-                logs.see(tk.END)
-
-            with torch.no_grad():
-                for idx, (x, y) in enumerate(test_Loader):
-                    x = x.to(device, torch.float32)
-                    y = y.to(device).unsqueeze(1)
-                    if event_stop != None:
-                        if event_stop.is_set():
-                            print("stop happend!!!!")
-                            break
-
-                    num_correct = 0
-                    num_pixel = 0
-                    
-                    preds = torch.sigmoid(model(x))
-                    
-                    p1 = (preds <= 0.5)
-                    p2 = (preds > 0.4)
-                    preds_1 = torch.where(p1 & p2, 150, 0)
-                    
-
-                    p1 = (preds <= 0.4)
-                    p2 = (preds > 0.35)
-                    preds_2 = torch.where(p1 & p2, 100, 0)
-
-                    p1 = (preds <= 0.35)
-                    p2 = (preds > 0.3)
-                    preds_3 = torch.where(p1 & p2, 50, 0)
-
-                    preds = torch.where(preds > 0.5, 255, 0)
-
-                    preds = preds + preds_1 + preds_2 + preds_3
-                    # print("y ",(y == 255).sum(), (y == 0).sum())
-                    # print("preds ",( preds == 255).sum(), (preds == 0).sum())
-                    preds = np.clip(preds.cpu(), 0, 255)
-
-                    num_correct = (preds == y.cpu()).sum()
-                    num_pixel = y.numel()
-
-                    # print(f"num_correct: {num_correct}, num_pixel: {num_pixel}\n")
-                    print(f"* accurracy => {(num_correct/num_pixel)*100}%\n")
-                    if logs != None:
-                        logs.configure(state="normal")
-                        logs.insert(tk.END, f"[+] accurracy => {(num_correct/num_pixel)*100}%\n")
-                        logs.configure(state="disabled")
-                        logs.see(tk.END)
-                    
-                    
-                    ######################################################################
-                    #####################        輸出測試圖片       #######################
-                    ######################################################################
-
-                    # preds = np.clip(preds + preds_1, 0, 255)
-
-                    if show_dir != '':
-                        for i, img in enumerate(preds):                               
-                            if idx == 0 and i < 5:
-                                img = img.permute(1, 2, 0)
-                                cv.imwrite(show_dir+'/{}e{}b{}.jpg'.format(epoch, idx+1, i+1), img.numpy())      # 預測遮罩
-                        for i, imgx in enumerate(x.cpu()):
-                            if idx == 0 and i < 5 and epoch == 1:
-                                imgx = imgx.permute(1, 2, 0)
-                                cv.imwrite(show_dir+'/_original{}.jpg'.format(i+1), imgx.numpy())            # 原圖
-                        for i, imgy in enumerate(y.cpu()):
-                            if idx == 0 and i < 5 and epoch == 1:
-                                imgy = imgy.permute(1, 2, 0)
-                                cv.imwrite(show_dir+'/__target{}.jpg'.format(i+1), imgy.numpy())           # 原遮罩
-                        # for i, tumor in enumerate(x):
-                        #     if idx == 0 and i < 5:
-                        #         tumor = tumor.permute(1, 2, 0)
-                        #         mask = torch.where(preds[i] > 200, 0, 255) 
-                        #         mask = mask.permute(1, 2, 0)
-                        #         tumor = torch.clip(tumor + mask, 0, 255)
-                        #         cv.imwrite(show_dir+'/{}e{}b{}_t.jpg'.format(epoch, idx+1, i+1), tumor.numpy())
-                    
-                    ######################################################################
-            # writer.add_scalar('accurracy %', (num_correct/num_pixel)*100, epoch)
-    except Exception as e:
-        print("Error!!!")
-        if logs != None:
-            logs.configure(state="normal")
-            logs.insert(tk.END, "[-] ")
-            logs.insert(tk.END, e)
-            logs.insert(tk.END, "\n")
-            logs.configure(state="disabled")
-
-def set_segmentation_model( 
-            model,
-            device="cpu",
-            batch_size=10,
-            lr=0.0001,
-            full_img="",
-            full_mask="",
-            img_height=224,
-            img_width=224,
-            lit_n=0,
-            load=''
-           ):
+    #### load model parameter ####
     if load != '':
         Load(model, torch.load(load))
         print(f"load file from {load}")
@@ -474,34 +269,13 @@ def set_segmentation_model(
     segmentation = {
         "train_Loader": train_Loader,
         "test_Loader": test_Loader,
-        "optimizer": test_Loader,
+        "optimizer": optimizer,
         "loss_f": loss_f,
         "model": model,
-
+        "show_dir": show_dir,
+        "save": save
     }
-    return segmentation
-
-def train_epoch(
-    model,
-    device="cpu",
-    train_Loader='',
-    loss_f='',
-    optimizer='',
-):
-    epoch_loss = 0
-    for idx, (data, target) in enumerate(train_Loader):
-        data = data.to(device, torch.float32)
-        target = target.to(device, torch.float32).unsqueeze(1)
-        prediction = model(data)
-        target = torch.where(target > 127, 1., 0.)
-        loss = loss_f(prediction, target)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-        print(f"\t\tBatch {idx+1} done, with loss = {loss}")
-    checkpoint = {'state_dict': model.state_dict(), 'optmizer': optimizer.state_dict()}
-    return checkpoint, epoch_loss/len(train_Loader)
+    return set
 
 def train_test(
     model,
@@ -552,8 +326,49 @@ def train_test(
                         imgy = imgy.permute(1, 2, 0)
                         cv.imwrite(show_dir+'/__target{}.jpg'.format(i+1), imgy.numpy())           # 原遮罩  
 
-        return 0
-
+def train_loop(
+    set,
+    device="cpu",
+    epochs=0,
+    logs='',
+    stop=None
+):
+    try:
+        for epoch in range(1, epochs+1):
+            if stop.is_set() and stop != None:
+                log_record(logs, f"[*] epoch stop")
+                break
+            epoch_loss = 0
+            for idx, (data, target) in enumerate(set["train_Loader"]):
+                if stop.is_set() and stop != None:
+                    log_record(logs, f"[*] optimize stop")
+                    break
+                data = data.to(device, torch.float32)
+                target = target.to(device, torch.float32).unsqueeze(1)
+                prediction = set["model"](data)
+                target = torch.where(target > 127, 1., 0.)
+                loss = set["loss_f"](prediction, target)
+                set["optimizer"].zero_grad()
+                loss.backward()
+                set["optimizer"].step()
+                epoch_loss += loss.item()
+                print(f"\t\tBatch {idx+1} done, with loss = {loss}")
+                log_record(logs, f"[+] Batch {idx+1} done, with loss = {loss}")
+            checkpoint = {'state_dict': set["model"].state_dict(), 'optmizer': set["optimizer"].state_dict()}
+            # 還沒弄判斷要不要保存參數的函式
+            train_test(
+                set["model"],
+                device,
+                set["test_Loader"],
+                set["show_dir"],
+                epoch
+            )
+            log_record(logs, f"[+] epoch {epoch+1} done, with loss = {epoch_loss/len(set['train_Loader'])}")
+        log_record(logs, f"[+] END")
+    except Exception as e:
+         print("Error!!!")  
+         log_record(logs, f"[-] {e}")
+    
 def Totest( model,
             load_file,
             test_dir,
